58a37e2cbcd3b746f9144ee83fce92aec978bd54
Dwinatasya <dwinatasya45.11@gmail.com>
2021-06-22 12:41:50 +0800

    kaoskupedia

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php //membuat koneksi
+  $host = "localhost";
+  $user = "root";
+  $password = "";
+  $database = "kaoskupedia";
+
+  $con = mysqli_connect($host, $user, $password, $database);
+?>
